749e1c01041a1efb35b085ea11d8c07bbd0bd5f8
Kyle Pu <kylez.pu@gmail.com>
2022-02-25 14:33:35 -0800

    Bare bones pages

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./HomePage.css"
+
+function HomePage() {
+  return (
+    <p>Hi!</p>
+  );
+}
+
+export default HomePage;
---
+++ src/components/Resume/Resume.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Resume.css"
+
+function Resume() {
+  return (
+    <p>RESUME HERE</p>
+  );
+}
+
+export default Resume;
