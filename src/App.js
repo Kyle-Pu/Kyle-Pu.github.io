d774b433827ef2c87ecbfeeaaf59b1cbf68b90d8
Kyle Pu <kylez.pu@gmail.com>
2022-02-27 16:26:53 -0800

    Buttons styled with Tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import HomePage from "./components/HomePage/HomePage"
 import Resume from "./components/Resume/Resume"
+import "./App.css"
 
 import {
   BrowserRouter,
@@ -13,15 +14,12 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-        <nav
-          style={{
-            borderBottom: "solid 1px",
-            paddingBottom: "1rem",
-          }}
-        >
-          <Link to="/Home">Home</Link> |{" "}
-          <Link to="/Resume">Resume</Link>
-        </nav>
+
+        <div className='absolute right-0'>
+          <Link to="/Home" className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Home</Link>
+          {" "}
+          <Link to="/Resume" className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Resume</Link>
+        </div>
 
         <Routes>
           <Route path="/" element={<HomePage />} />
