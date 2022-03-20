a734aa05b72f3f8a22ae16df105a697ef0fc219e
Kyle Pu <kylez.pu@gmail.com>
2022-03-20 00:44:10 -0700

    Plans for blog post format

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -2,6 +2,12 @@ import React from 'react'
 import "./Blog.css"
 
 function Blog() {
+  //  TODO: would be ideal if I can pull the data from text files written separately for each blog post
+  // Format for blog posts:
+  //  First line: Title
+  //  Second line: Date
+  //  Lines Onward: Blog Post
+
   return (
     <h1>Hi!</h1>
   );
