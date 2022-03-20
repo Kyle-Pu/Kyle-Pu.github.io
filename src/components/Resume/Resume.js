9eb321b9b6a5a2763ff8ea84b4a968bbd72ad256
Kyle Pu <kylez.pu@gmail.com>
2022-03-20 00:44:55 -0700

    Structure laid out, open to change

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -2,9 +2,21 @@ import React from 'react'
 import "./Resume.css"
 
 function Resume() {
+  let titles = ["Research Assistant @ Whitney Lab, UC Berkeley", "Research Assistant @ Pellegrini Lab, UCLA", "Summer Research Fellow, National Institutes of Health"]; // Titles of work experiences
+  let timelines = ["Spring to Summer", "August to Fall", "Winter to Spring Quarter"]; // Times of each activity
+  let descriptions = ["Outline shit", "Do shit", "Look at shit"]; // More details
+
   return (
     <div class="Container">
-      <iframe src={require("../../data/resume.pdf")} width="100%" height="100%"/>
+      {titles.map((title, index) => {
+        return (
+          <div>
+            <p class="job-title">{title}</p>
+            <p class="job-times">{timelines[index]}</p>
+            <p class="job-description">{descriptions[index]}</p>
+          </div>
+        )
+      })}
     </div>
   );
 }
