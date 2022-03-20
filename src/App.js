1a4ea9d6f5bf4500cad51132dfcd9c296d91ba3e
Kyle Pu <kylez.pu@gmail.com>
2022-03-20 01:04:42 -0700

    New tab for food recommendations

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import HomePage from "./components/HomePage/HomePage"
 import Resume from "./components/Resume/Resume"
 import Blog from "./components/Blog/Blog"
+import Food from "./components/Food/Food"
 import "./App.css"
 
 import {
@@ -22,6 +23,8 @@ function App() {
             <Link to="/Resume" class="NavButton">Résumé</Link>
             {" "}
             <Link to="/Blog" class="NavButton">Blog</Link>
+            {" "}
+            <Link to="/Food" class="NavButton">Food Recs</Link>
           </div>
 
           <div class="Logo">
@@ -36,6 +39,7 @@ function App() {
           <Route path="/Home" element={<HomePage />} />
           <Route path="Resume" element={<Resume />} />
           <Route path="Blog" element={<Blog />} />
+          <Route path="Food" element={<Food />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/Food/Food.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Food.css"
+
+function Food() {
+    return (
+        <div class="Container">
+            <p>Near UCLA: Nothern Cafe, Mr. Noodle</p>
+        </div>
+    );
+}
+
+export default Food;
