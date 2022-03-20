03af4c5bbc53a11d2e4573cd689829d3355382ab
Kyle Pu <kylez.pu@gmail.com>
2022-03-20 00:57:59 -0700

    Home page basics done

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -5,7 +5,12 @@ function HomePage() {
   return (
     <div class="Container">
       <div class="Picture">
-        <img src={require("../../imgs/pfp.jpeg")} alt="Profile picture" />
+        <img src={require("../../imgs/pfp.png")} alt="Profile picture" />
+      </div>
+      <div class="Caption">
+        <p>2nd Year CS @ UCLA</p>
+        <p class="Question">Where am I here?</p>
+        <p class="Answer">Off Rodeo Drive in Beverly Hills! No, I don't live there. Yes, I wish I lived there.</p>
       </div>
     </div>
   );
