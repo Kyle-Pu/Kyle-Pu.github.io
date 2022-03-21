5c1fcb110b2829515bbbd7b7277ac0c340c84fb1
Kyle Pu <kylez.pu@gmail.com>
2022-03-20 22:57:38 -0700

    Remove old comments detailing plans for blog post data retrieval

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -3,13 +3,6 @@ import "./Blog.css"
 import blogposts from "../blogposts"
 
 function Blog() {
-  //  TODO: would be ideal if I can pull the data from text files written separately for each blog post
-  // Format for blog posts:
-  //  First line: Title
-  //  Second line: Date (sort blog posts by date here)
-  //  Third line: Tags/topics
-  //  Lines Onward: Blog Post
-  // Note: can't use `fs` because GitHub Pages only supports static sites
   let titles = blogposts.map((bPost) => bPost.title)
   let dates = blogposts.map((bPost) => bPost.date)
   let tags = blogposts.map((bPost) => bPost.tags)
