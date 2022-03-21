ea94437fc1cce6118fc5e09bf7fc7d594c29f156
Kyle Pu <kylez.pu@gmail.com>
2022-03-20 22:56:23 -0700

    First iteration of blog posts design done.
    In the future, would like to add ways to hide each blog post, table of contents, filters based on tags, etc.

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,15 +1,34 @@
 import React from 'react'
 import "./Blog.css"
+import blogposts from "../blogposts"
 
 function Blog() {
   //  TODO: would be ideal if I can pull the data from text files written separately for each blog post
   // Format for blog posts:
   //  First line: Title
-  //  Second line: Date
+  //  Second line: Date (sort blog posts by date here)
+  //  Third line: Tags/topics
   //  Lines Onward: Blog Post
-
+  // Note: can't use `fs` because GitHub Pages only supports static sites
+  let titles = blogposts.map((bPost) => bPost.title)
+  let dates = blogposts.map((bPost) => bPost.date)
+  let tags = blogposts.map((bPost) => bPost.tags)
+  let content = blogposts.map((bPost) => bPost.content)
+  
   return (
-    <h1>Hi!</h1>
+    <div className="Container">
+      {titles.map((title, index) => {
+        return (
+          <div className="BPost" key="index">
+            <h1 className="Title">{title}</h1>
+            <h2 className="Date">{dates[index]}</h2>
+            <h2 className="Tag"> #{tags[index].join(" #")}</h2>
+            {content[index]}
+            <hr></hr>
+          </div>
+        )
+      })}
+    </div>
   );
 }
 
