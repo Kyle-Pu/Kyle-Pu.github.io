90f6c64f9f606d13a04824042e98f21877dead09
Kyle Pu <kylez.pu@gmail.com>
2022-03-22 12:02:04 -0700

    Basic website complete. This is like v1 without a resume

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -2,9 +2,9 @@ import React from 'react'
 import "./Resume.css"
 
 function Resume() {
-  let titles = ["Research Assistant @ Whitney Lab, UC Berkeley", "Research Assistant @ Pellegrini Lab, UCLA", "Summer Research Fellow, National Institutes of Health"]; // Titles of work experiences
-  let timelines = ["Spring to Summer", "August to Fall", "Winter to Spring Quarter"]; // Times of each activity
-  let descriptions = ["Outline shit", "Do shit", "Look at shit"]; // More details
+  let titles = ["Coming soon!"]; // Titles of work experiences
+  let timelines = []; // Times of each activity
+  let descriptions = [""]; // More details
 
   return (
     <div className="Container">
