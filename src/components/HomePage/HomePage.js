8675cfbbaca0d4a0afd80ec065e39f49c3d9a8dd
Kyle Pu <kylez.pu@gmail.com>
2022-03-22 00:36:46 -0700

    Big changes!
    No longer have question and answer format for where the picture is taken. It's instead included as an easter egg when visitors hover over my picture.
    
    Added links to professional sites on the home page as well.

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -5,12 +5,12 @@ function HomePage() {
   return (
     <div className="Container">
       <div className="Picture">
-        <img src={require("../../imgs/pfp.png")} alt="Profile picture" />
+        <img className="pfp" src={require("../../imgs/pfp.png")} alt="Profile picture" />
+        <p className="EasterEgg">Off Rodeo Drive in Beverly Hills! No, I don't live there. Yes, I wish I lived there.</p>
       </div>
       <div className="Caption">
         <p>2nd Year CS @ UCLA</p>
-        <p className="Question">Where am I here?</p>
-        <p className="Answer">Off Rodeo Drive in Beverly Hills! No, I don't live there. Yes, I wish I lived there.</p>
+        <p><a href="https://github.com/kyle-pu" target="_blank">GitHub</a> | <a href="https://linkedin.com/in/kyle-pu" target="_blank">LinkedIn</a> | <a href="https://scholar.google.com/citations?user=1nlDj9UAAAAJ&hl=en" target="_blank">Google Scholar</a></p>
       </div>
     </div>
   );
