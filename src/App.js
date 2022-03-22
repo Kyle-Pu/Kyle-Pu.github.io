3b49e4289d87a242168cee0d213edbf46e79af03
Kyle Pu <kylez.pu@gmail.com>
2022-03-22 15:01:05 -0700

    Projects page
    Create component for each project display and add in 3 main projects.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import HomePage from "./components/HomePage/HomePage"
 import Resume from "./components/Resume/Resume"
+import Projects from "./components/Projects/Projects"
 import Blog from "./components/Blog/Blog"
 import "./App.css"
 
@@ -19,9 +20,9 @@ function App() {
       <BrowserRouter>
         <div className="Wrapper">
           <div className="NavContainer">
+            <Link to="/Projects" className="NavButton">Projects</Link>
             <Link to="/Blog" className="NavButton">Blog</Link>
           </div>
-
           <div className="Logo">
             <Link to="/Home" className="NavButton">Kyle Pu</Link>
           </div>
@@ -33,6 +34,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/Home" element={<HomePage />} />
           <Route path="Resume" element={<Resume />} />
+          <Route path="Projects" element={<Projects />} />
           <Route path="Blog" element={<Blog />} />
         </Routes>
       </BrowserRouter>
---
+++ src/components/Projects/ProjectFrame.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function ProjectFrame(props) {
+    // props.links is an object containing all links of the specific project
+    let linkTitles = [];
+    let linkPaths = [];
+    for(const site in props.links){
+        linkTitles.push(site);
+        linkPaths.push(props.links[site]);
+    }
+
+    return (
+        <figure>
+            <br/>
+            <img className="Thumbnail" src={props.imgPath} />
+            <figcaption><b>{props.title}</b> {props.caption} {props.award != "" && <i>{props.award}</i>}</figcaption>
+            <div className="LinksContainer">
+                {linkTitles.map((title, index) => {
+                    let sep = "|";
+                    if(index == linkPaths.length - 1){
+                        sep = ""; // Don't put a separator after the last link
+                    }
+                    return <a href={linkPaths[index]} target="_blank"> {title} {sep}</a>
+                })}
+            </div>
+            <br/>
+        </figure>
+    );
+}
+
+export default ProjectFrame;
